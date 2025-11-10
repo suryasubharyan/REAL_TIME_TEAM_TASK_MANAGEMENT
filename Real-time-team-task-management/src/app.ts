7ed316c0c319e171ea3ff505a8de2724ea509c28
suryasubbharyan <suryasubharyan@gmail.com>
2025-11-10 11:31:18 +0530

    Fix: corrected CORS origin (removed trailing slash)

--- Real-time-team-task-management/src/app.ts
+++ Real-time-team-task-management/src/app.ts
@@ -19,7 +19,7 @@ connectDB();
 const app = express();
 app.use(cors({
   origin: [
-    "https://frontend-isaq.onrender.com/", // 🧠 replace with your actual Vercel URL
+    "https://frontend-isaq.onrender.com", // 🧠 replace with your actual Vercel URL
     "http://localhost:5173", // optional: for local testing (Vite)
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
